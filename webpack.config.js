9415dee7e3d5069d05fb90d7851b01d09972def7
Oxana Oleynikova <oleynikova.oxana@gmail.com>
2022-12-28 01:38:54 +0100

    feat: add copyWebpackPlugin and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -19,9 +20,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
-      // favicon: './assets/img/favicon.ico',
+      favicon: './assets/img/favicon.ico',
     }),
     new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './assets/img/webp', to: './assets/image' }
+      ]
+    }),
     new MiniCssExtractPlugin({
       filename: 'style.css'
     })
